11255bc541b2a16b726e89a34e89039cf6a2f184
Pablo G. Perusina <pagarcia@merkleinc.com>
2020-10-04 21:32:31 -0600

    Created build bash file for docker image

---
+++ server1/docker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker login
+docker build \
+    --no-cache \
+    -t server1 \
+    --build-arg username=$1 \
+    --build-arg password=$2 \
+    .
+docker tag server1 $3/server1
+docker push $3/server1
