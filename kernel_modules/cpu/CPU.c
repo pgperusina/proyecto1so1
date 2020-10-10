a193634ae808c3c03a2aa865070af02c4aa5a505
Pablo G. Perusina <pagarcia@merkleinc.com>
2020-10-10 13:44:03 -0600

    create function to format

--- kernel_modules/cpu/CPU.c
+++ kernel_modules/cpu/CPU.c
@@ -82,6 +82,27 @@ static u64 get_iowait_time(int cpu)
 
 #endif
 
+static u64 nsec_to_clock_t(u64 x)
+{
+	if (NSEC_PER_SEC % USER_HZ == 0)
+	{
+		return div_u64(user, NSEC_PER_SEC / USER_HZ);
+	}
+	else if (USER_HZ % 512 == 0)
+	{
+		return div_u64(user * USER_HZ / 512, NSEC_PER_SEC / 512);
+	}
+	else
+	{
+		/*
+					* max relative error 5.7e-8 (1.8s per year) for USER_HZ <= 1024,
+					* overflow after 64.99 years.
+					* exact for HZ=60, 72, 90, 120, 144, 180, 300, 600, 900, ...
+					*/
+		return div_u64(user * 9, (9ull * NSEC_PER_SEC + (USER_HZ / 2)) / USER_HZ);
+	}
+}
+
 static int show_stat(struct seq_file *p, void *v)
 {
 	int i, j;
@@ -112,26 +133,8 @@ static int show_stat(struct seq_file *p, void *v)
 
 	total = user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice;
 
-	if (NSEC_PER_SEC % USER_HZ == 0)
-	{
-		user = div_u64(user, NSEC_PER_SEC / USER_HZ);
-	}
-	else if (USER_HZ % 512 == 0)
-	{
-		user = div_u64(user * USER_HZ / 512, NSEC_PER_SEC / 512);
-	}
-	else
-	{
-		/*
-					* max relative error 5.7e-8 (1.8s per year) for USER_HZ <= 1024,
-					* overflow after 64.99 years.
-					* exact for HZ=60, 72, 90, 120, 144, 180, 300, 600, 900, ...
-					*/
-		user = div_u64(user * 9, (9ull * NSEC_PER_SEC + (USER_HZ / 2)) / USER_HZ);
-	}
-
 	seq_printf(p, "All \t");
-	seq_printf(p, "%llu", user);
+	seq_printf(p, "%llu", nsec_to_clock_t(user);
 	seq_printf(p, "\t ");
 	seq_printf(p, "%llu", system);
 	seq_printf(p, "\t");
