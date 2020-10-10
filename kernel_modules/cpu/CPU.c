31709dbdd18cdc96c23bcfb3bdc26313750f094a
Pablo G. Perusina <pagarcia@merkleinc.com>
2020-10-10 13:26:44 -0600

    fix

--- kernel_modules/cpu/CPU.c
+++ kernel_modules/cpu/CPU.c
@@ -82,22 +82,6 @@ static u64 get_iowait_time(int cpu)
 
 #endif
 
-u64 nsec_to_clock_t(u64 x)
-{
-#if (NSEC_PER_SEC % USER_HZ) == 0
-	return div_u64(x, NSEC_PER_SEC / USER_HZ);
-#elif (USER_HZ % 512) == 0
-	return div_u64(x * USER_HZ / 512, NSEC_PER_SEC / 512);
-#else
-	/*
-         * max relative error 5.7e-8 (1.8s per year) for USER_HZ <= 1024,
-         * overflow after 64.99 years.
-         * exact for HZ=60, 72, 90, 120, 144, 180, 300, 600, 900, ...
-         */
-	return div_u64(x * 9, (9ull * NSEC_PER_SEC + (USER_HZ / 2)) / USER_HZ);
-#endif
-}
-
 static int show_stat(struct seq_file *p, void *v)
 {
 	int i, j;
@@ -127,6 +111,24 @@ static int show_stat(struct seq_file *p, void *v)
 	}
 
 	total = user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice;
+
+	if (NSEC_PER_SEC % USER_HZ == 0)
+	{
+		user = div_u64(user, NSEC_PER_SEC / USER_HZ);
+	}
+	else if (USER_HZ % 512 == 0)
+	{
+		use = div_u64(user * USER_HZ / 512, NSEC_PER_SEC / 512);
+	}
+	else
+	{
+		/*
+					* max relative error 5.7e-8 (1.8s per year) for USER_HZ <= 1024,
+					* overflow after 64.99 years.
+					* exact for HZ=60, 72, 90, 120, 144, 180, 300, 600, 900, ...
+					*/
+		user = div_u64(user * 9, (9ull * NSEC_PER_SEC + (USER_HZ / 2)) / USER_HZ);
+	}
 
 	seq_put_decimal_ull(p, ' ', nsec_to_clock_t(user));
 	seq_printf(p, "All \t");
